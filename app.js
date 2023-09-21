bb51144b91a429a90f50052bf6a02d6ee8225a8a
Mehmet Ugur Satir <musatir@gmail.com>
2023-09-21 22:45:27 +0200

    app.js setup2

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const port = process.env.PORT || 5000;
 const start = async ()=>{
     try {
         app.listen(port, ()=>{
-            console.log(`Server is listening on port ${port}`)
+            console.log(`Server is listening on port ${port}...`)
         })
     } catch (error) {
-        
+        console.log(error)
     }
 }
 
