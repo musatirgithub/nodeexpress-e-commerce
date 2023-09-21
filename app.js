6d70f4fec6cbeeaa5a0fbee89c8357822b4f6818
Mehmet Ugur Satir <musatir@gmail.com>
2023-09-21 22:43:54 +0200

    basic app.js setup

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('E-Commerce API');
+const express = require('express');
+const app = express();
+const connectDB = require('./db/connect');
+
+const port = process.env.PORT || 5000;
+
+const start = async ()=>{
+    try {
+        app.listen(port, ()=>{
+            console.log(`Server is listening on port ${port}`)
+        })
+    } catch (error) {
+        
+    }
+}
+
+start();
