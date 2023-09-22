06e81aead41036a41c58dab46a3ec43cedb773f5
Mehmet Ugur Satir <musatir@gmail.com>
2023-09-22 22:07:14 +0200

    validator packages isEmail func used for email val

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const validator = require('validator');
 
 const UserSchema = new mongoose.Schema({
     name:{
@@ -11,6 +11,10 @@ const UserSchema = new mongoose.Schema({
     email:{
         type:String,
         required:[true, 'Please provide email'],
+        validate:{
+            validator:validator(),
+            message:'Please provide a valid email',
+        }
     },
     password:{
         type:String,
