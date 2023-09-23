193fd1d9a0be850d11f1af31f47b1c1233ad2ace
Mehmet Ugur Satir <musatir@gmail.com>
2023-09-23 14:49:26 +0200

    register functionality updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,21 @@
 const User = require('../models/user');
 const {StatusCodes} = require('http-status-codes');
 const CustomError = require('../errors');
+const jwt = require('jsonwebtoken');
 
 const register = async (req,res)=>{
-    const user = await User.create(req.body)
+    const {email, name, password} = req.body;
+    const emailAlreadyExists = await User.findOne({email});
+
+    if(emailAlreadyExists){
+        throw new CustomError.BadRequestError('Email already exists');
+    }
+
+    // first registered user is an admin
+    const isFirstAccount = await User.countDocuments({}) === 0;
+    const role = isFirstAccount? "admin":"user";
+
+    const user = await User.create({email, name, password, role})
 
     if(!user){
         throw new CustomError.BadRequestError('')
