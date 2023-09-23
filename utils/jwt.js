e768ea4e66d772d3bda9d25ac91860d8c5253646
Mehmet Ugur Satir <musatir@gmail.com>
2023-09-23 15:17:20 +0200

    isTokenValid util constructed

--- utils/jwt.js
+++ utils/jwt.js
@@ -5,3 +5,8 @@ const createJWT = ({payload})=>{
     const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn:process.env-JWT_LIFETIME})
     return token
 }
+
+
+const isTokenValid = ({token})=> jwt.verify(token, process.env.JWT_SECRET);
+
+module.exports = {createJWT, isTokenValid}
