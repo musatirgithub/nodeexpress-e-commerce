1ac733e0f2c5fdc3cc53d94b79a08c78c6024dba
Mehmet Ugur Satir <musatir@gmail.com>
2023-09-26 15:05:50 +0200

    authenticateUser middleware basic structure

---
+++ middleware/authentication.js
@@ -0,0 +1,12 @@
+const CustomError = require('../errors');
+const {isTokenValid} =require('../utils');
+
+const authenticateUser = async (req, res, next)=>{
+    const token = req.signedCookies.token;
+    if(!token){
+        console.log('error, no token present')
+        // throw new CustomError.UnauthenticatedError('')
+    }
+    console.log('error, no token present')
+    next();
+}
