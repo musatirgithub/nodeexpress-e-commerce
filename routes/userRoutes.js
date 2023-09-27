3513d7aa8bad82a5c71d8da8f9a5ea14bb8d1b6a
Mehmet Ugur Satir <musatir@gmail.com>
2023-09-27 16:08:18 +0200

    minor bug fix

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ const {getAllUsers, getSingleUser, showCurrentUser, updateUser, updateUserPasswo
 const {authenticateUser, authorizePermissions} = require('../middleware/authentication');
 
 router.route('/').get(authenticateUser, authorizePermissions('admin', 'owner'), getAllUsers);
-router.route('/showMe').get(showCurrentUser);
+router.route('/showMe').get(authenticateUser, showCurrentUser);
 router.route('/updateUser').patch(authenticateUser, updateUser);
 router.route('/updateUserPassword').patch(authenticateUser, updateUserPassword);
 router.route('/:id').get(authenticateUser, getSingleUser)
