eb3c94bb1ba15ccc7b39c7262dc11af3436626ad
Mehmet Ugur Satir <musatir@gmail.com>
2023-09-29 00:05:58 +0200

    ProductSchema 5

--- models/product.js
+++ models/product.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-    name:{type:String, trim:true, required:[true, 'Please provide product name'], maxLength:[100, 'Product name cannot excess 100 characters']},
-    price:{type:Number},
-    description:{type:String},
+    name:{
+        type:String, 
+        trim:true, 
+        required:[true, 'Please provide product name'], 
+        maxLength:[100, 'Product name cannot excess 100 characters'],
+    },
+    price:{
+        type:Number, 
+        required:[true, 'Please provide product price'], 
+        default:0,
+    },
+    description:{
+        type:String, 
+        trim:true, 
+        required:[true, 'Please provide product description'], 
+        maxLength:[1000, 'Description cannot excess 1000 characters'],
+    },
     image:{type:String},
     category:{type:String},
     company:{type:String},
