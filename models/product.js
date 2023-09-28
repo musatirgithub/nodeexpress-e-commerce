ee3dfb058d56ffd262842af586d5c49a01f0064c
Mehmet Ugur Satir <musatir@gmail.com>
2023-09-29 00:35:25 +0200

    ProductSchema 10

--- models/product.js
+++ models/product.js
@@ -39,9 +39,26 @@ const ProductSchema = new mongoose.Schema({
         type:[String], 
         required:true, 
     },
-    featured:{type:Boolean},
-    freeShipping:{type:Boolean},
-    inventory:{type:Number},
-    averageRating:{type:Number},
-    user:
-})
+    featured:{
+        type:Boolean,
+        default:false, 
+    },
+    freeShipping:{
+        type:Boolean,
+        default:false, 
+    },
+    inventory:{
+        type:Number,
+        required:true,
+        default:15, 
+    },
+    averageRating:{
+        type:Number,
+        default:0, 
+    },
+    user:{
+        type:mongoose.Types.ObjectId,
+        ref:'User',
+        required:true,
+    },
+}, {timestamps:true})
