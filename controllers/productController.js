b05582291b189df00782e6f476a6af10f7c818a2
Mehmet Ugur Satir <musatir@gmail.com>
2023-10-02 23:39:15 +0200

    minor fix

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,21 +1,21 @@
 
 
 const createProduct = async (req,res)=>{
-    send('Create Product')
+    res.send('Create Product')
 }
 const getAllProducts = async (req,res)=>{
-    send('Get all products')
+    res.send('Get all products')
 }
 const getSingleProduct = async (req,res)=>{
-    send('Get single products')
+    res.send('Get single products')
 }
 const updateProduct = async (req,res)=>{
-    send('Update Product')
+    res.send('Update Product')
 }
 const deleteProduct = async (req,res)=>{
-    send('Delete Product')
+    res.send('Delete Product')
 }
 const uploadImage = async (req,res)=>{
-    send('Upload Image')
+    res.send('Upload Image')
 }
 module.exports = {createProduct, getAllProducts, getSingleProduct, updateProduct, deleteProduct, uploadImage}
