35dc85d384a3a4f1b6943a3a3f2bb8407736a36e
Mehmet Ugur Satir <musatir@gmail.com>
2023-10-02 23:31:41 +0200

    productRoutes.js basic structure

---
+++ routes/productRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const {createProduct, getAllProducts, getSingleProduct, updateProduct, deleteProduct, uploadImage} = require('../controllers/productController');
+
+
+router.route('/').get(getAllProducts);
+router.route('/:id').get(getSingleProduct);
