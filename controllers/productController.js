a2fe05bf40f78b3c65796dac67edfd1ede4d9993
Mehmet Ugur Satir <musatir@gmail.com>
2023-10-02 23:24:25 +0200

    controller func boilerPlates

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,3 +3,18 @@
 const createProduct = async (req,res)=>{
     send('Create Product')
 }
+const getAllProducts = async (req,res)=>{
+    send('Get all products')
+}
+const getSingleProduct = async (req,res)=>{
+    send('Get single products')
+}
+const updateProduct = async (req,res)=>{
+    send('Update Product')
+}
+const deleteProduct = async (req,res)=>{
+    send('Delete Product')
+}
+const uploadImage = async (req,res)=>{
+    send('Upload Image')
+}
