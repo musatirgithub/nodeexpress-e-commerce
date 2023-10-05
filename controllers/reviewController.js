b5a1d896febe42da056ec438b934001ed753e4cc
Mehmet Ugur Satir <musatir@gmail.com>
2023-10-05 17:06:07 +0200

    createReview func added

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,14 +1,29 @@
+const Review = require('../models/review');
+const Product = require('../models/product');
 const {StatusCodes} = require('http-status-codes');
 const CustomError = require('../errors');
-
+const {checkPermissions} = require('../utils');
 
 const createReview = async (req,res)=>{
-    res.send('create review')
+    const {product:productId} = req.body;
+    const isValidProduct = await Product.findOne({_id:productId});
+    if (!isValidProduct){
+        throw new CustomError.NotFoundError(`No product with the id: ${productId}`)
+    }
+
+    const alreadySubmitted = await Review.findOne({product:productId, user:req.user.userId})
+    if(alreadySubmitted){
+        throw new CustomError.BadRequestError('Already submitted a review for this product')
+    }
+    req.body.user = req.user.userId;
+
+    const review = await Review.create(req.body);
+    res.status(StatusCodes.CREATED).json({review});
 }
 
 const getAllReviews = async (req,res)=>{
-    console.log('Hello there')
-    res.send('get all reviews')
+
+    res.status(StatusCodes.OK).json({reviews});
 }
 
 const getSingleReview = async (req,res)=>{
