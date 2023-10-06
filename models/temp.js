9e23113f6cfcd178a75a9447f3c83c09a632b6f0
Mehmet Ugur Satir <musatir@gmail.com>
2023-10-07 00:01:03 +0200

    temp.js

---
+++ models/temp.js
@@ -0,0 +1,36 @@
+import { MongoClient } from 'mongodb';
+import {
+  ObjectId
+} from 'mongodb';
+
+/*
+ * Requires the MongoDB Node.js Driver
+ * https://mongodb.github.io/node-mongodb-native
+ */
+
+const agg = [
+  {
+    '$match': {
+      'product': new ObjectId('65207b70df66037f14cdc45d')
+    }
+  }, {
+    '$group': {
+      '_id': null, 
+      'averageRating': {
+        '$avg': '$rating'
+      }, 
+      'numOfReviews': {
+        '$sum': 1
+      }
+    }
+  }
+];
+
+const client = await MongoClient.connect(
+  '',
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+const coll = client.db('E-COMMERCE').collection('reviews');
+const cursor = coll.aggregate(agg);
+const result = await cursor.toArray();
+await client.close();
