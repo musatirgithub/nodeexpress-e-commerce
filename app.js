b85c6f6d95eec76b1c77e75b01f2f93b0b143b43
Mehmet Ugur Satir <musatir@gmail.com>
2023-10-08 22:13:36 +0200

    morgan commented out

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const app = express();
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload'); 
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
 
 // routers
 const authRouter = require('./routes/authRoutes');
@@ -24,23 +29,23 @@ const connectDB = require('./db/connect');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(rateLimiter({
+    windowMs: 15 * 1000 * 60,
+    max: 60,
+}));
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 
-app.use(morgan('tiny'));
+// app.use(morgan('tiny'));   we used morgan during development phase
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
 app.use(express.static('./public'));
 app.use(fileUpload());
 
-app.get('/', (req,res)=>{
-    res.send('e-commerce api')
-})
-
-app.get('/api/v1', (req,res)=>{
-    // console.log(req.cookies);
-    console.log(req.signedCookies);
-    res.send('e-commerce api')
-})
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
